0de97ed42db5b7d2afaf44ede6613ea754284ceb
mortargoblin <aaro.euro@gmail.com>
2026-06-12 23:24:30 +0300

    planet spawning improved

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <stdbool.h>
+
 #include <raylib.h>
 #include <raymath.h>
 
@@ -24,25 +26,17 @@ void apply_gravity(Body *body1, Body *body2, float dt) {
     float dist_sq = dir.x * dir.x + dir.y * dir.y;
     float dist = sqrtf(dist_sq);
 
-    if (dist < 0.001f) return;
+    if (dist < 0.01f) return;
 
     dir.x /= dist;
     dir.y /= dist;
     
     float G = 50.0f; // mielivaltainen taikaluku :DD
     float accel_mag = G * body2->mass / dist_sq;
-    Vector2 accel = {
-      dir.x * accel_mag,
-      dir.y * accel_mag
-    };
+    Vector2 accel = {dir.x * accel_mag, dir.y * accel_mag};
 
     body1->vel.x += accel.x * dt;
     body1->vel.y += accel.y * dt;
-
-    /*
-    body1->pos.x += body1->vel.x * dt;
-    body1->pos.y += body1->vel.y * dt;
-    */
 }
 
 void draw_body(const Body *body) {
@@ -53,6 +47,22 @@ void draw_velocity_line(Body *body) {
   float x = body->pos.x + body->vel.x;
   float y = body->pos.y + body->vel.y;
   DrawLine(body->pos.x, body->pos.y, x, y, WHITE);
+}
+
+void spawn_planet(Body *bodies, int *body_count) {
+  float vel_x = ((float)rand() / RAND_MAX) * 200 - 100;
+  float vel_y = ((float)rand() / RAND_MAX) * 200 - 100;
+
+  Body new_planet = {
+    .pos = GetMousePosition(),
+    .vel = {vel_x, vel_y},
+    .size = 6,
+    .mass = 20,
+    .color = WHITE
+  };
+
+  bodies[*body_count] = new_planet;
+  (*body_count)++;
 }
 
 int main(int argc, char **argv) {
@@ -71,7 +81,7 @@ int main(int argc, char **argv) {
   const Vector2 screen_center = {(float)SCREEN_WIDTH / 2, (float)SCREEN_HEIGHT / 2};
   Body bodies[] = {
     // size, mass, pos, vel, color
-    {10, 10000, screen_center, {0, 0}, YELLOW}, // sun
+    {14, 10000, screen_center, {0, 0}, YELLOW}, // sun
     {4, 8, Vector2Subtract(screen_center, (Vector2){80, 0}), {0, 65}, RED},
     {5, 10, Vector2Subtract(screen_center, (Vector2){120, 0}), {0, 60}, GREEN},
     {7, 20, Vector2Subtract(screen_center, (Vector2){-150, 0}), {0, -50}, PURPLE},
@@ -95,47 +105,21 @@ int main(int argc, char **argv) {
       bodies[i].pos.y += bodies[i].vel.y * dt;
     }
 
-    // INPUT
-    static Vector2 last_mouse = {0};
-    Vector2 mouse = GetMousePosition();
-
+    /// INPUT
     if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
-
-      Vector2 delta = Vector2Subtract(mouse, last_mouse);
-
       for (int i = 0; i < body_count; i++) { // :DD
-        bodies[i].pos = Vector2Add(bodies[i].pos, delta);
+        bodies[i].pos = Vector2Add(bodies[i].pos, GetMouseDelta());
       }
-
     }
-    last_mouse = mouse;
 
     if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-      printf("CLICKED\n");
-
-      Body new_planet;
-      new_planet.pos = GetMousePosition();
-      new_planet.vel = (Vector2){-30, 0};
-      new_planet.size = 10;
-      new_planet.mass = 20;
-      new_planet.color = WHITE;
-
-      bodies[body_count] = new_planet;
-      body_count++;
-      printf("body_count: %d\n", body_count);
+      spawn_planet(bodies, &body_count);
     }
 
     /// RENDERING
     BeginDrawing();
     {
       ClearBackground(BLACK);
-      DrawText(
-          TextFormat(
-            "FRAME TIME\n%f\n\nEARTH VELOCITY\nX: %.2f\nY: %.2f",
-            dt, bodies[1].vel.x, bodies[1].vel.y
-            ),
-          10, 10, 20, WHITE
-          );
 
       for (int i = 0; i < body_count; i++) {
         draw_body(&bodies[i]);
@@ -147,6 +131,15 @@ int main(int argc, char **argv) {
         }
       }
 
+      DrawText(
+          TextFormat(
+            "RMB to move camera\nLMB to spawn planet\n\n"
+            "FRAME TIME\n%f\n\nBODIES\n%d\n\n"
+            "EARTH VELOCITY\nX: %.2f\nY: %.2f",
+            body_count, dt, bodies[1].vel.x, bodies[1].vel.y
+            ),
+          10, 10, 20, WHITE
+          );
     }
     EndDrawing();
   }
